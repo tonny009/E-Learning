415aea652c9e9662f3000ce185469e0d20e4cc29
tonny009 <tabassum.islam009@gmail.com>
2022-10-26 18:08:18 +0900

    react to pdf download applied

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -7,13 +7,18 @@ import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import './CourseDetails.css'
 import { Link } from 'react-router-dom';
-import ReactToPrint from 'react-to-print';
-import Home from '../Home/Home'
+import ReactToPrint, { useReactToPrint } from 'react-to-print';
+
 
 
 const CourseDetails = () => {
 
-    const componentRef = useRef();
+    const componentRef = useRef(null);
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+    });
+
+
 
     const courseDetails = useLoaderData();
     // console.log(courseDetails[0].name)
@@ -27,20 +32,23 @@ const CourseDetails = () => {
         <Container>
             <Row>
                 <Col lg="8">
-                    <div>
+                    <div >
                         <Card style={{ width: '30rem', backgroundColor: '#D3D3D3' }}>
                             <Card.Img className='w-100' variant="top" src={image} />
                             <Card.Body>
                                 <Card.Title>Course Title: {name}</Card.Title>
-                                <Card.Text>
-                                    <h6>About Course:</h6>
+                                <div ref={componentRef}>
+                                    <Card.Text>
+                                        <h6>About Course:</h6>
 
-                                    <p>{Course_description}</p>
-                                </Card.Text>
-                                <Card.Text>
-                                    <p>Ratings: {ratings}</p>
-                                    <p>Total Enrolled: {enrolled}</p>
-                                </Card.Text>
+                                        <p>{Course_description}</p>
+                                    </Card.Text>
+                                    <Card.Text>
+                                        <p>Ratings: {ratings}</p>
+                                        <p>Total Enrolled: {enrolled}</p>
+                                    </Card.Text>
+                                </div>
+
                                 <Button variant="success"><Link className='premium-link' to={`/checkout/${id}`}>Get Premium Access</Link></Button>
                             </Card.Body>
                         </Card>
@@ -48,14 +56,16 @@ const CourseDetails = () => {
                 </Col>
                 <Col lg="4">
                     <div className='dwnld-box mt-4 pt-4 px-4'>
-                        <h3>Click This Button to download PDF:</h3>
 
-                        <button className='pdf-button'>Download PDF</button>
+
+                        <h3>Click This Button to download PDF:</h3>
+                        <button className='pdf-button' onClick={handlePrint}>Download PDF</button>
+                        {/* <button className='pdf-button'>Download PDF</button> */}
                     </div>
 
                 </Col>
             </Row>
-        </Container>
+        </Container >
     );
 };
 
